62927d9e6e347738e05a536e18ee01e7a3565ff7
ShifaHasan <manazir.java@gmail.com>
2019-08-16 19:43:08 +0530

    BTHorizentalDistance

---
+++ src/main/java/BinaryTrees/BTHorizontalDistance.java
@@ -0,0 +1,52 @@
+package BinaryTrees;
+
+import java.util.*;
+
+public class BTHorizontalDistance {
+    public static void main (String[] args) {
+        System.out.println("Hello Akhrat");
+        BTUtilBuilder btUtil = new BTUtilBuilder();
+        BT bt = btUtil.createFullBT();
+        BTHorizontalDistance btHorizentalDistance = new BTHorizontalDistance();
+        Map<Integer, List<BT>> horizontalDistMap = new HashMap<Integer, List<BT>>();
+        horizontalDistMap = btHorizentalDistance.horizontalDistance(bt, 0, horizontalDistMap);
+        System.out.println(horizontalDistMap);
+    }
+
+    private Map<Integer, List<BT>> horizontalDistance (BT bt, int horizontalLenght, Map<Integer, List<BT>> horizontalDistMap) {
+
+        if (bt == null)
+            return horizontalDistMap;
+
+        if (horizontalDistMap.containsKey(horizontalLenght)) {
+            List<BT> listBT = horizontalDistMap.get(horizontalLenght);
+            listBT.add(bt);
+        } else {
+            List<BT> listBt = new LinkedList<BT>();
+            listBt.add(bt);
+            horizontalDistMap.put(horizontalLenght, listBt);
+        }
+        horizontalDistance(bt.getLeft(), horizontalLenght - 1, horizontalDistMap);
+        horizontalDistance(bt.getRight(), horizontalLenght + 1, horizontalDistMap);
+
+        return horizontalDistMap;
+    }
+}
+
+ /*                    A1
+
+            B2              C3
+
+      D4        E5      F6        G7
+                                        I9
+
+-(-2)-----(-1)--------0------+1---+2----+3---------------------------
+  D4       B2     A1,E5,F6   C3   G7    I9
+0=[BT{data='A', numData=1}, BT{data='E', numData=5}, BT{data='F', numData=6}],
+-1=[BT{data='B', numData=2}],
+-2=[BT{data='D', numData=4}],
+1=[BT{data='C', numData=3}],
+2=[BT{data='G', numData=7}],
+3=[BT{data='I', numData=9}]}
+
+*/
