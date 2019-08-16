c59df1a391d96f3985ca64d11b56b25d92d32d13
ShifaHasan <manazir.java@gmail.com>
2019-08-16 20:02:04 +0530

    BTHorizentalDistance

--- src/main/java/BinaryTrees/BTHorizontalDistanceOrVerticalOrder.java
+++ src/main/java/BinaryTrees/BTHorizontalDistanceOrVerticalOrder.java
@@ -2,12 +2,12 @@ package BinaryTrees;
 
 import java.util.*;
 
-public class BTHorizontalDistance {
+public class BTHorizontalDistanceOrVerticalOrder {
     public static void main (String[] args) {
         System.out.println("Hello Akhrat");
         BTUtilBuilder btUtil = new BTUtilBuilder();
         BT bt = btUtil.createFullBT();
-        BTHorizontalDistance btHorizentalDistance = new BTHorizontalDistance();
+        BTHorizontalDistanceOrVerticalOrder btHorizentalDistance = new BTHorizontalDistanceOrVerticalOrder();
         Map<Integer, List<BT>> horizontalDistMap = new HashMap<Integer, List<BT>>();
         horizontalDistMap = btHorizentalDistance.horizontalDistance(bt, 0, horizontalDistMap);
         System.out.println(horizontalDistMap);
@@ -41,7 +41,8 @@ public class BTHorizontalDistance {
                                         I9
 
 -(-2)-----(-1)--------0------+1---+2----+3---------------------------
-  D4       B2     A1,E5,F6   C3   G7    I9
+  D4       B2     A1,E5,F6   C3   G7    I9  --vertical order
+
 0=[BT{data='A', numData=1}, BT{data='E', numData=5}, BT{data='F', numData=6}],
 -1=[BT{data='B', numData=2}],
 -2=[BT{data='D', numData=4}],
@@ -49,4 +50,6 @@ public class BTHorizontalDistance {
 2=[BT{data='G', numData=7}],
 3=[BT{data='I', numData=9}]}
 
+all list contain in vertical order
+sort and print list in order
 */
