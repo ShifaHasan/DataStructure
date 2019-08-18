1c6530bf9aa001823f2f7a6f5dddbd4bf7cf6f10
ShifaHasan <manazir.java@gmail.com>
2019-08-19 00:31:12 +0530

    HeapCommit

---
+++ src/main/java/BinaryTreesHeap/Find3ElementSuchaipulsajISaK.java
@@ -0,0 +1,10 @@
+package BinaryTreesHeap;
+
+//Given an array, find 3 element such that a[i]+a[j]=a[k]
+//i/p:3,5,1,2,9,4
+//o/p a[1]+a[5] = a[4]
+//     5  + 4   = 9
+public class Find3ElementSuchaipulsajISaK {
+
+
+}
